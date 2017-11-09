1d03767a13db3c2758c92720ae459d5b4b2b2c23
longxingren <973506608@qq.com>
2017-11-10 00:28:27 +0800

    init

---
+++ src/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="ckeditor">
+    <div :id="id"></div>
+  </div>
+</template>
+<script>
+  let time = new Date().getTime()
+
+  export default {
+    name: 'vue-ckeditor',
+    props: {
+      id: {
+        type: String,
+        default: () => `editor-${time}`
+      },
+      config: {
+        type: Object,
+        default: () => {}
+      }
+    },
+    mounted () {
+      if(!window.CKEDITOR){
+        console.warn('CKeditor is missing')
+      } else {
+        window.CKEDITOR.replace(this.id, this.config)
+      }
+    }
+
+  }
+</script>
